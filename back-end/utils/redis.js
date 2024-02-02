1534d813f5f62d512f0f02b444678c9c7d03a661
Arinze Nwutobo <tim.arinze@outlook.com>
2024-02-02 01:56:17 +0100

    redis

--- back-end/utils/redis.js
+++ back-end/utils/redis.js
@@ -3,55 +3,49 @@ require('dotenv').config();
 
 class RedisClient {
   constructor() {
-    // Create a new Redis client
-    // this.client = redis.createClient({
-    //   host: process.env.REDIS_HOST,
-    //   port: process.env.REDIS_PORT,
-    // });
-    this.client = redis.createClient(process.env.REDIS_EXTERNAL_URL || process.env.REDIS_INTERNAL_URL);
+    this.client = redis.createClient({host: 'localhost', port: 6379});
 
     // Log Redis errors to the console
     this.client.on('error', (error) => {
       console.error('Redis Error:', error)
     });
   }
-
-    // isAlive returns true if the connection is Ok or false otherwise
-    isAlive() {
-      if (this.client.connected) {
-        console.log("Redis is alive")
-        return true;
-      }
-      return false;
+  // isAlive returns true if the connection is Ok or false otherwise
+  isAlive() {
+    if (this.client.connected) {
+      console.log("Redis is alive")
+      return true;
     }
+    return false;
+  }
 
     // get returns the value of a key in Redis
-    async get(key) {
-      return new Promise((resolve, reject) => {
-        this.client.get(key, (err, data) => {
-          if (err) reject(err);
-          resolve(data);
-        });
-      
+  async get(key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (err, data) => {
+        if (err) reject(err);
+        resolve(data);
       });
-    };
+      
+    });
+  };
 
-    // set saves a key value pair in Redis for a given time in seconds
-    async set(key, value, duration) {
-      return new Promise((resolve, reject) => {
-        this.client.set(key, value, 'EX', duration, (err, data) => {
-          if (err) reject(err);
-          resolve(data);
+  // set saves a key value pair in Redis for a given time in seconds
+  async set(key, value, duration) {
+    return new Promise((resolve, reject) => {
+      this.client.set(key, value, 'EX', duration, (err, data) => {
+        if (err) reject(err);
+        resolve(data);
         });
       });
     }
 
-    // del deletes a key in Redis
-    async del(key) {
-      return new Promise((resolve, reject) => {
-        this.client.del(key, (err, data) => {
-          if (err) reject(err);
-          resolve(data);
+  // del deletes a key in Redis
+  async del(key) {
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (err, data) => {
+        if (err) reject(err);
+        resolve(data);
         });
       })
     }
